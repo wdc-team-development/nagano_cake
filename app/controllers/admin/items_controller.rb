c2633cdcdcbe93a4351101ea1dec73bb52cc403f
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-02-15 16:27:51 +0000

    admin/itemの空のview作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,23 @@
 class Admin::ItemsController < ApplicationController
+    
+    def index
+        @items = Item.all
+    end 
+    
+    def show 
+        @item = Item.find(params:[id])
+    end 
+    
+    def new
+    end 
+    
+    def create
+    end 
+    
+    def edit
+    end 
+    
+    def update
+    end 
+
 end
