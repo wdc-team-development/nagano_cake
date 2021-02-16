b95b73bdf76dbac742c15d10268e00875cf1218c
MochizukiShunju <b1302475@gmail.com>
2021-02-16 07:24:49 +0000

    Bootstrap追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,19 @@
 class CustomersController < ApplicationController
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def unsubscribe
+    @customer = current_customer
+  end
+  private
+
+  def user_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted, :create_at, :update_at)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'homes/top'
   end
+  get 'customers/show' => 'customers#show'
+  get 'customers/edit' => 'customers#edit'
+  get 'customers/unsubscribe' => 'customers#unsubscribe'
+  resources :customers, only: [:withdraw, :unsubscribe, :update]
   devise_for :admins
   devise_for :customers
 end
